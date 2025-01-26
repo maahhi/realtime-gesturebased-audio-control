e7859f4f55657c0d2f470a8c4946e68a0fd251cf
abahrani <ali.bahrani94521036@gmail.com>
2025-01-25 19:26:54 -0800

    adding notes to jsonparser-bodygesture

--- jsonparser-bodygesture.py
+++ jsonparser-bodygesture.py
@@ -1,8 +1,20 @@
+'''
+This file contains functions to convert gesture data stored in JSON format into a CSV file,
+which is commonly used for machine learning training. It is important to note that all data
+related to hand gestures is excluded and will not be present in the resulting CSV file.
+'''
+
 import json
 import pandas as pd
 
 
 def load_json_objects(filename):
+    '''
+    Loads the json data into the memory and cleans it for the targeted training purpose.
+
+    :param filename: the location of the json file in the file system.
+    :return: a list of the dictionaries parsed from the json file.
+    '''
     with open(filename, 'r') as file:
         content = file.read()
 
@@ -10,7 +22,7 @@ def load_json_objects(filename):
     obj_buffer = ''
     brace_count = 0
     in_object = False
-
+    #ToDo use library instead of making the function longer and harder to read.
     for char in content:
         if char == '{':
             brace_count += 1
@@ -37,7 +49,12 @@ def load_json_objects(filename):
     return objects
 
 def bodyfeatures(filename):
-    print('filename:',filename)
+    '''
+    Loads the json data into the memory and cleans it for the targeted training purpose.
+
+    :param filename: the location of the json file in the file system. 
+    :return: a pandas' dataframe in memory
+    '''
     pairs = ['shoulder', 'elbow', 'wrist', 'pinky', 'index', 'hip', 'knee', 'heel', 'foot_index']
 
     #concat 2 list
@@ -64,17 +81,20 @@ def bodyfeatures(filename):
 
 
 def bodygesture_jsontocsv(gesture_name):
+    '''
+    create a csv file for a specific body gesture that has already been saved into a json file.
+    :param gesture_name: for every gesture name there must be a <gesture_name>.json file or it will cause an error
+    '''
     bfdf = bodyfeatures(gesture_name)
-
-    #print(bfdf.head())
-    #is there any missing data?
-    #print(bfdf.isnull().sum())
     print('done')
     #save to csv
     bfdf.to_csv(gesture_name+'.csv',index=False)
-# bg = ['a_right_hand_up','a_right_leg_up','a_left_hand_up','a_left_leg_up','a_nutral_stand']
-# bg = ['gesture_class_0', 'gesture_class_1', 'gesture_class_2', 'gesture_class_3', 'gesture_class_4', 'gesture_class_5']
-bg = ["am_ghezi"]
-for i in range(len(bg)):
-    print(bg[i])
-    bodygesture_jsontocsv(bg[i])
+
+
+if __name__ == "__main__": # this file may act both as a library or a main entrypoint.
+    # bg = ['a_right_hand_up','a_right_leg_up','a_left_hand_up','a_left_leg_up','a_nutral_stand']
+    # bg = ['gesture_class_0', 'gesture_class_1', 'gesture_class_2', 'gesture_class_3', 'gesture_class_4', 'gesture_class_5']
+    bg = ["am_ghezi"]
+    for i in range(len(bg)):
+        print(bg[i])
+        bodygesture_jsontocsv(bg[i])
