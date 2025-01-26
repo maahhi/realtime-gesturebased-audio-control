3be1a43fd813ae98c3f8553ddd1a618b35414407
abahrani <ali.bahrani94521036@gmail.com>
2025-01-26 11:53:24 -0800

    create entrypoint for osc-dict-receiver file

--- osc-dict-receiver.py
+++ osc-dict-receiver.py
@@ -1,25 +1,17 @@
 from pythonosc import dispatcher
 from pythonosc import osc_server
 import json
+import os
 from util.json2dataseries import j2ds
 import pandas as pd
-# Define the IP and port to listen on
-ip = "127.0.0.1"  # Localhost (or change to your local IP for external connections)
-port = 8000       # Port must match what is set in Max MSP
 
 
 # Function to store JSON strings in a file
 def save_json_string(json_string, file_path="tempdata.json"):
     # remove all the next lines in the string
     json_string = json_string.replace("\n", "")
-
     with open(file_path, "a") as file:  # "a" mode to append each JSON string as a new line
         file.write(json_string + "\n")
-
-#if tempdata.json exist, remove it
-import os
-if os.path.exists("tempdata.json"):
-    os.remove("tempdata.json")
 
 
 # Define a handler function to process incoming OSC messages
@@ -54,15 +46,24 @@ def save_csv(address, filepath):
         df.to_csv(filepath+".csv", index=False)
     samples.clear()
 
-# Set up the dispatcher to map the OSC address to the handler
-disp = dispatcher.Dispatcher()
-disp.map("/dictData", json_dict_handler)  # Must match the OSC address set in Max
-#disp.map("/save", save_handler)  # Must match the OSC address set in Max
-disp.map("/save", save_csv)
 
-# Start the server to listen for incoming OSC messages
-server = osc_server.ThreadingOSCUDPServer((ip, port), disp)
-print(f"Listening on {ip}:{port}")
 
-# Keep the server running
-server.serve_forever()
+if __name__ == "__main__":
+    # Define the IP and port to listen on
+    ip = "127.0.0.1"  # Localhost (or change to your local IP for external connections)
+    port = 8000       # Port must match what is set in Max MSP
+
+    if os.path.exists("tempdata.json"):
+        os.remove("tempdata.json")
+    # Set up the dispatcher to map the OSC address to the handler
+    disp = dispatcher.Dispatcher()
+    disp.map("/dictData", json_dict_handler)  # Must match the OSC address set in Max
+    #disp.map("/save", save_handler)  # Must match the OSC address set in Max
+    disp.map("/save", save_csv)
+
+    # Start the server to listen for incoming OSC messages
+    server = osc_server.ThreadingOSCUDPServer((ip, port), disp)
+    print(f"Listening on {ip}:{port}")
+
+    # Keep the server running
+    server.serve_forever()
