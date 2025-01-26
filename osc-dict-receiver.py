f8535d29fa6fbfee141f8d98e75084d124e19126
abahrani <ali.bahrani94521036@gmail.com>
2025-01-26 12:15:45 -0800

    adding comments to osc-dict-receiver.py

--- osc-dict-receiver.py
+++ osc-dict-receiver.py
@@ -6,46 +6,37 @@ from util.json2dataseries import j2ds
 import pandas as pd
 
 
-# Function to store JSON strings in a file
-def save_json_string(json_string, file_path="tempdata.json"):
-    # remove all the next lines in the string
-    json_string = json_string.replace("\n", "")
-    with open(file_path, "a") as file:  # "a" mode to append each JSON string as a new line
-        file.write(json_string + "\n")
+samples = [] # This variable is global as it is used both json_dict_handler and save_csv 
 
 
-# Define a handler function to process incoming OSC messages
-samples = []
 def json_dict_handler(address, json_string):
+    '''
+    This function is a handler for the server that adds data to the gloabl sample list
+
+    :param address: the endpoint that the message is sent to. (not used)
+    :param json_string: the body gesture sent from MaxMSP in string format
+    '''
     # Decode the JSON string back into a dictionary
     dictionary = json.loads(json_string)
     print("Received dictionary:", dictionary)
     ds = j2ds(dictionary)
     print(ds)
-    #save_json_string(json_string)
     samples.append(ds)
 
 
-
-def save_handler(address, filepath):
-    # if the filepath exist, append the temp file to filepath
-    if os.path.exists(filepath+".json"):
-        with open("tempdata.json", "r") as temp_file:
-            with open(filepath+".json", "a") as file:
-                for line in temp_file:
-                    file.write(line)
-        os.remove("tempdata.json")
-    else:
-        os.rename("tempdata.json", filepath+".json")
-
-def save_csv(address, filepath):
+def save_csv(address, file_path):
+    '''
+    This function is a handler that saves all the items in global list 'sample'. The function clears the 
+    sample list after a successful save.
+    :param address: the endpoint that the message is sent to. (not used)
+    :param file_path: the file_path is sent without the extention so .csv will be concatinated to the path_file.
+    '''
     df = pd.DataFrame(samples)
-    if os.path.exists(filepath + ".csv"):
-        df.to_csv(filepath + ".csv", mode='a', header=False, index=False)
+    if os.path.exists(file_path + ".csv"):
+        df.to_csv(file_path + ".csv", mode='a', header=False, index=False)
     else:
-        df.to_csv(filepath+".csv", index=False)
+        df.to_csv(file_path+".csv", index=False)
     samples.clear()
-
 
 
 if __name__ == "__main__":
@@ -58,7 +49,6 @@ if __name__ == "__main__":
     # Set up the dispatcher to map the OSC address to the handler
     disp = dispatcher.Dispatcher()
     disp.map("/dictData", json_dict_handler)  # Must match the OSC address set in Max
-    #disp.map("/save", save_handler)  # Must match the OSC address set in Max
     disp.map("/save", save_csv)
 
     # Start the server to listen for incoming OSC messages
